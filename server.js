0f42b0f6f725857a2f2a6285ed667031b122d6e7
unknown <petergramenides@gmail.com>
2020-04-25 05:40:47 -0400

    nearly finished backend
    pretty much everything is done. I just need someone to help me with the async call of getUserInformation() and I can replace the "username" with "session_id" throughout the api.

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ function add_page(request, response)
     });
 }
 
-// TODO: logo out the user with session
+// adds page to page_collection
 app.get('/api/pages/add', function(request, response)
 {
 	var owner = String(request.query.owner);
@@ -130,16 +130,133 @@ app.get('/api/pages/edit', function(request, response)
 	  }).catch(error => console.log("Error finding the username."));
 });
 
-// logs out the user with session
+function getUserInformation(session_id)
+{
+	account_collection.find({}).toArray(function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+        	var all_users = result;
+        	for (var x = 0; x < all_users.length; ++x)
+        	{
+        		for (var y = 0; y < all_users[x].sessions.length; ++y)
+        		{
+        			var found = bcrypt.compareSync(session_id, all_users[x].sessions[y]);
+        			if (found == true)
+        			{
+        				console.log(all_users[x].username);
+   						return all_users[x].username;
+        			}
+        		}
+        	}
+        }
+    });
+}
+
+// get user information from session id
+app.get('/api/account/user', function(request, response)
+{
+	var session_id = String(request.query.session_id);
+	account_collection.find({}).toArray(function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+        	var all_users = result;
+        	for (var x = 0; x < all_users.length; ++x)
+        	{
+        		for (var y = 0; y < all_users[x].sessions.length; ++y)
+        		{
+        			var found = bcrypt.compareSync(session_id, all_users[x].sessions[y]);
+        			if (found == true)
+        			{
+        				response.header("X-Content-Type-Options", "nosniff");
+   						return response.jsonp({"success": all_users[x].username});
+        			}
+        		}
+        	}
+        }
+    });
+});
+
+function account_logout(session_id, sessions, request, response)
+{
+	var delete_index = -1;
+	for (var x = 0; x < sessions.length; ++x)
+	{
+		var result = bcrypt.compareSync(session_id, sessions[x]);
+		if (result == true)
+		{
+			delete_index = x;
+		}
+	}
+	if (delete_index > -1) {
+		sessions.splice(delete_index, 1);
+	}
+	account_collection.updateOne(
+	   { "username": request.query.username },
+	   {
+	     $set: { "sessions": sessions },
+	     $currentDate: { lastModified: true }
+	   }
+	);
+	console.log("Successfully logged out user " + request.query.username);
+	response.header("X-Content-Type-Options", "nosniff");
+   	return response.jsonp({"success": "success"});
+}
+
+// logs user out with session_id
 app.get('/api/account/logout', function(request, response)
 {
-
+	var session_id = String(request.query.session_id);
+	var username = String(request.query.username);
+	var sessions;
+	account_collection.findOne({ "username": username })
+		.then(account_data => {
+	    if(account_data) {
+	    	sessions = account_data.sessions;
+	    	account_logout(session_id,sessions, request, response);
+	    }
+	  }).catch(error => console.log("Error finding the username."));
 });
 
 // logs in the user with session
 app.get('/api/account/login', function(request, response)
 {
+	var username = String(request.query.username);
+	var password = String(request.query.password);
 
+	account_collection.findOne({ "username": username })
+		.then(account_data => {
+	    if(account_data) {
+	    	bcrypt.genSalt(saltRounds, function(error, salt) {
+		    	bcrypt.compare(password, account_data.password, function(err, result) {
+				    if (result == true)
+				    {
+				    	console.log("The password is correct.")
+				    	var session_id = uuidv4();
+				    	bcrypt.hash(session_id, salt, function(error, hash) {
+				    		var sessions = account_data.sessions;
+					    	sessions.push(hash);
+					    	account_collection.updateOne(
+							   { "username": username },
+							   {
+							     $set: { "sessions": sessions },
+							     $currentDate: { lastModified: true }
+							   }
+							);
+					    	console.log("Successfully logged in " + username + " with new session.");
+					    	response.header("X-Content-Type-Options", "nosniff");
+				    		return response.jsonp({"success": session_id});
+						});
+				    } else {
+				    	console.log("The password or confirmation was incorrect.")
+			    		response.header("X-Content-Type-Options", "nosniff");
+			    		return response.jsonp({"error": "error"});
+				    }
+				});
+			});
+	    }
+	  }).catch(error => console.log("Error finding the username."));
 });
 
 // removes a like to a post
@@ -242,39 +359,112 @@ app.get('/api/pages/posts/likes/add', function(request, response)
 // returns all comments
 app.get('/api/pages/posts/comments', function(request, response)
 {
+	var post_id = String(request.query.post_id);
+	var page = String(request.query.page);
 
+	page_collection.findOne({ "name": page })
+		.then(result => {
+	    if(result) {
+	    	var all_comments = result.posts[post_id].comments;
+	    	var all_engagements = result.posts[post_id].engagements;
+	    	var comments = 
+	    	{
+	    		"comments": all_comments,
+	    		"engagements": all_engagements
+	    	}
+	    	console.log("Successfully return data from post_id " + post_id + " to page " + page);
+    		response.header("X-Content-Type-Options", "nosniff");
+			return response.jsonp({"success": comments});
+	    } else {
+	    	console.log("Could not find data for page " + page);
+	    	return response.jsonp({"error": "error"});
+	    }
+	  }).catch(err => console.log(err));
 });
-
-// edits a comment
-app.get('/api/pages/posts/comments/edit', function(request, response)
-{
-
-});
-
 
 // deletes a comments
 app.get('/api/pages/posts/comments/delete', function(request, response)
 {
+	var post_id = String(request.query.post_id);
+	var username = String(request.query.username);
+	var page = String(request.query.page);
+	var comment = String(request.query.comment);
 
+	page_collection.findOne({ "name": page })
+		.then(result => {
+	    if(result) {
+	    	var all_posts = result.posts;
+	    	if (all_posts[post_id].comments.includes(comment))
+	    	{
+	    		var updated_comments = [];
+	    		var updated_engagements = all_posts[post_id].engagements;
+	    		var engagement_index = -1;
+	    		for (var x = 0; x < all_posts[post_id].comments.length; ++x)
+	    		{
+	    			if (all_posts[post_id].comments[x] != comment)
+	    			{
+	    				updated_comments.push(all_posts[post_id].comments[x]);
+	    				engagement_index = x;
+	    			}
+	    		} all_posts[post_id].comments = updated_comments;
+	    	} else {
+	    		console.log("User " + username + " already deleted comment for post " + post_id);
+	    		return response.jsonp({"error": "error"});
+	    	}
+
+	    	if (engagement_index > -1) {
+	    		updated_engagements.splice(engagement_index, 1);
+			} all_posts[post_id].engagements = updated_engagements;
+
+	    	  page_collection.updateOne(
+			   { "name": page },
+			   {
+			     $set: { "posts": all_posts },
+			     $currentDate: { lastModified: true }
+			   }
+			);
+	    	console.log(username + " successfully removed comment from post " + post_id + " on page " + page);
+    		response.header("X-Content-Type-Options", "nosniff");
+			return response.jsonp({"success": "success"});
+	    } else {
+	    	console.log("Could not find data for page " + page);
+	    	return response.jsonp({"error": "error"});
+	    }
+	  }).catch(err => console.log(err));
 });
 
 // adds a comment
 app.get('/api/pages/posts/comments/add', function(request, response)
 {
-	// TODO: session handling, do login and logout
-	// get session id and save in its own collection
-	// hash the session ids
-	// make a function that handles all that and returns the
-	// account information, but specifically the username
-	// remove username from most commands and replace it with
-	// the session ID! also add a function to get user info
-	// from the sessionid and return it to the frontend.
+	var post_id = String(request.query.post_id);
+	var username = String(request.query.username);
+	var page = String(request.query.page);
+	var comment = String(request.query.comment);
 
-	// for comments - copy and paste from likes and make
-	// some small changes. Same goes for followers.
+	page_collection.findOne({ "name": page })
+		.then(result => {
+	    if(result) {
+	    	var all_posts = result.posts;
+    		all_posts[post_id].engagements.push(username);
+    		all_posts[post_id].comments.push(comment);
+	    	  page_collection.updateOne(
+			   { "name": page },
+			   {
+			     $set: { "posts": all_posts },
+			     $currentDate: { lastModified: true }
+			   }
+			);
+	    	console.log("Successfully added comment from user " + username + " to post " + post_id + " on page " + page);
+    		response.header("X-Content-Type-Options", "nosniff");
+			return response.jsonp({"success": "success"});
+	    } else {
+	    	console.log("Could not find data for page " + page);
+	    	return response.jsonp({"error": "error"});
+	    }
+	  }).catch(err => console.log(err));
 });
 
-// edits a post, TODO: implement likes and comments
+// edits a post on a page
 app.get('/api/pages/posts/edit', function(request, response)
 {
 	var username = String(request.query.username);
@@ -305,10 +495,12 @@ app.get('/api/pages/posts/edit', function(request, response)
 	    if(result) {
 	    	var all_posts = result.posts;
 	    	var likes = result.posts.post_id.likes;
-	    	//var comments = result.posts.post_id.comments;
+	    	var comments = result.posts.post_id.comments;
+	    	var engagements = result.posts.post_id.engagements;
 	    	all_posts[post_id] = updated_post;
 	    	console.log(all_posts);
-	    	//all_posts[post_id].comments = comments;
+	    	all_posts[post_id].comments = comments;
+	    	all_posts[post_id].engagements = engagements;
 	    	all_posts[post_id].likes = likes;
 	    	page_collection.updateOne(
 			   { "name": page },
@@ -404,7 +596,6 @@ app.get('/api/pages/posts/add', function(request, response)
 	    	return response.jsonp({"error": error});
 	    }
 	  }).catch(err => console.log(err));
-
 });
 
 // gets all the posts for a page
@@ -440,43 +631,85 @@ app.get('/api/pages', function(request, response)
 // deletes a follower to the user
 app.get('/api/account/followers/delete', function(request, response)
 {
+	var session_id = String(request.query.session_id);
+	var username = getUserInformation(session_id);
+	var follower = String(request.query.follower);
 
+	account_collection.findOne({ "username": username })
+		.then(result => {
+	    if(result) {
+	    	var followers = result.followers;
+	    	if (followers.includes(follower))
+	    	{
+	    		var updated_followers = [];
+	    		for (var x = 0; x < followers.length; ++x)
+	    		{
+	    			if (followers[x] != follower)
+	    			{
+	    				updated_followers.push(followers[x]);
+	    			}
+	    		} followers = updated_followers;
+	    	} else {
+	    		console.log("User " + follower + " already unfollowed " + username);
+	    		return response.jsonp({"error": "error"});
+	    	}
+	    	  account_collection.updateOne(
+			   { "username": username },
+			   {
+			     $set: { "followers": followers },
+			     $currentDate: { lastModified: true }
+			   }
+			);
+	    	console.log(follower + " successfully unfollowed user " + username);
+    		response.header("X-Content-Type-Options", "nosniff");
+			return response.jsonp({"success": "success"});
+	    } else {
+	    	console.log("Could not find data for page " + page);
+	    	return response.jsonp({"error": "error"});
+	    }
+	  }).catch(err => console.log(err));
 });
 
 // adds a follower to the user
 app.get('/api/account/followers/add', function(request, response)
 {
+	var session_id = String(request.query.session_id);
+	var username = getUserInformation(session_id);
+	var follower = String(request.query.follower);
 
-});
-
-// gets all the accounts a user is following
-app.get('/api/account/following', function(request, response)
-{
-
-});
-
-// deletes a user from the following list for an account
-app.get('/api/account/following/delete', function(request, response)
-{
-
-});
-
-// adds a user to the following list for an account
-app.get('/api/account/following/add', function(request, response)
-{
-
-});
-
-// gets all the users an account is following
-app.get('/api/account/following', function(request, response)
-{
-
+	account_collection.findOne({ "username": username })
+		.then(result => {
+	    if(result) {
+	    	var followers = result.followers;
+	    	if (!followers.includes(follower))
+	    	{
+	    		followers.push(follower);
+	    	} else {
+	    		console.log("User " + username + " already followed by " + follower);
+	    		return response.jsonp({"error": "error"});
+	    	}
+	    	  account_collection.updateOne(
+			   { "username": username },
+			   {
+			     $set: { "followers": followers },
+			     $currentDate: { lastModified: true }
+			   }
+			);
+	    	console.log("Successfully added follower " + follower + " to user " + username);
+    		response.header("X-Content-Type-Options", "nosniff");
+			return response.jsonp({"success": "success"});
+	    } else {
+	    	console.log("Could not find data for page " + page);
+	    	return response.jsonp({"error": "error"});
+	    }
+	  }).catch(err => console.log(err));
 });
 
 // gets all the account information
 app.get('/api/account', function(request, response)
 {
-	var username = String(request.query.username);
+	var session_id = String(request.query.session_id);
+	var username = getUserInformation(session_id);
 	console.log("Fetching all account information for username " + username);
 	account_collection.findOne({ "username": username })
 		.then(result => {
@@ -609,8 +842,8 @@ function create_account(request, response)
 	    		"first_name": first_name,
 	    		"last_name": last_name,
 	    		"birthday": birthday,
-	    		"followers": [],
-	    		"following": []
+	    		"sessions": [],
+	    		"followers": []
 	    	}
 	    	account_collection.insertOne(data, (error, result) => {
 		        if(error) {
@@ -626,7 +859,6 @@ function create_account(request, response)
 	});
 }
 
-// creates a new account, TODO: check if account/email already exists
 app.get('/api/account/create', function(request, response)
 {
 	console.log("Creating new account.");
@@ -677,7 +909,6 @@ app.listen(3000, function(){
         if(error) {
             throw error;
         }
-
         database = client.db(database_name);
         account_collection = database.collection("users");
         page_collection = database.collection("pages");
