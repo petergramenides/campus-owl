b22b444b115350c1c9b6ed0c5fe52b02221b1de3
Yaseen Mahmoud <mahmoy@rpi.edu>
2020-04-26 18:27:48 -0400

    Fixed front end dependencies

---
+++ frontend/src/app/components/sidenav/sidenav.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SidenavComponent } from './sidenav.component';
+
+@NgModule({
+    imports: [],
+    exports: [SidenavComponent],
+    declarations: [SidenavComponent],
+    providers: [],
+ })
+ 
+ export class SidenavModule {
+ }
