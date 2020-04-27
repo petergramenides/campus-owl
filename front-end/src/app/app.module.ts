1cf330379b8f25c938e6f63ef5c1e9cce737a05c
Daniel <schnolldj@gmail.com>
2020-04-27 16:27:13 -0400

    Refactored front end, added login sessions

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from "./home/home.component";
+import { MaterialModule } from "./material-module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
